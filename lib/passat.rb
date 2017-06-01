547be3ac2849a57785e6d36abc442721beea88db
atpons <taiga@atpons.com>
2017-06-01 19:23:48 +0900

    v0.2.0 release

--- lib/passat.rb
+++ lib/passat.rb
@@ -2,58 +2,73 @@ require "passat/version"
 
 module Passat
 
-  def conv(i)
-    if i == 1
-      return 0
-    else
-      return 1
-    end
+  def invert(i)
+  return not(i)
   end
 
-  class FromFile
-    include Passat
-    attr_accessor :x
+  attr_accessor :clause
 
-    def initialize(file)
-      @eq = []
-      begin
-        File.open(file) do |f|
-          f.each_line do |o|
-            @initial = "("
-            case o[0]
-            when "c"
-              puts "Comment"
-            when "p"
-              i = o.split()
-              @x = Array.new(i[2].to_i)
-              @y = i[3].to_i
-            else
-              o.split().each do |v|
-                if v.to_s[0] == "-"
-                  block = "conv(@x[" + (v.to_i + 1).abs.to_s + "])"
-                  @initial += (block + "|")
-                elsif v.to_s == "0"
-                  @initial.slice!(@initial.rindex("|"))
-                else
-                  block = "@x[" + (v.to_i - 1).to_s + "]"
-                  @initial += (block + "|")
-                end
-              end
-              @initial += ")"
-              @eq.push(@initial)
-            end
-          end
+  def load(file)
+    @clause = []
+     File.open(file) do |f|
+       f.each_line do |o|
+         @temparr = []
+         case o[0]
+         when "c"
+         when "p"
+         else
+           o.split("0").each do |x|
+             @temparr2 = []
+             x.split.each do |y|
+               @temparr2.push(y.to_i)
+             end
+             @temparr.push(@temparr2)
+           end
+           @temparr.each do |arr|
+             unless arr.empty?
+               @clause.push(arr)
+             end
+           end
+         end
+       end
+     end
+  end
+
+  class Boolean
+  include Passat
+    attr_accessor :boolean, :input
+    def initialize(input)
+      @input = input # input customize
+      @input.map! do |val|
+        if val == 0
+          val = false
+        elsif val == 1
+          val = true
         end
-
       end
     end
 
-    def solve(solution)
-      @x = solution
-      return eval(@eq.join("&"))
+    def convert()
+      @boolean = @clause.dup
+      @boolean.each_with_index do |a, i|
+        a.each_with_index do |b, j|
+          if b.to_s[0] == "-"
+            @boolean[i][j] = invert(@input[b.abs - 1])
+          else
+            @boolean[i][j] = @input[b.abs - 1]
+          end
+        end
+      end
     end
 
+    def check()
+      @formula = []
+      @boolean.each do |val|
+          @clause_formula = "(" + val.join("||") + ")"
+          @formula.push(@clause_formula)
+      end
+      return eval(@formula.join("&"))
+    end
   end
-
 
 end
